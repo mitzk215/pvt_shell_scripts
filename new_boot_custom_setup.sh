70cfb3ebb55bd67343faa820ed533d4a801d5d3b
mitzk215 <35113893+mitzk215@users.noreply.github.com>
2025-03-24 19:45:28 +0530

    Create new_boot_custom_setup.sh

---
+++ new_boot_custom_setup.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+# PURPOSE/DESCRIPTION : This is a single file consisting of all the necessary 
+# things I require when I start my ubuntu machine or restart my machine after a hang.
+
+#
+# FIRSTLY UBUNTU related changes has to be made by me!
+# 1. connect wifi
+# 2. change system date/time
+# 3. increase audio level
+
+# THEN FIrefox related changes have to be made  by me !
+# 1. Installing ad-blocke extensions, stylus
+# 2. enable cycle throug tabs.
+# 3. login to youtube automatically OR google login automatically
+# 4. login to github automatically.
+# 5. login to 
+
+# some default apps in ubuntu has to be removed like games,  thunderbird, etc.
+
+# e flag will exit the script as soon as first error or non-zero exit 
+# status is encountered
+set -e
+
+
+#### SNAP PACKAGE MANAGEMENT ######
+#testing
+sudo chmod a+x snap_tasks.sh
+.snap_tasks.sh
+# in case if I want to check the channel of firefox I can use the command:
+# snap info firefox
+sudo snap refresh && sudo snap refresh firefox channel latest/stable
+
+
+#### APT PACKAGE MANAGEMENT ####
+# testing
+sudo chmod a+x apt_tasks.sh
+.apt_tasks.sh
+
+# I also have to update thins in apt repository 
+sudo add_apt_repository -y universe
+# install packages from the apt repository which are not in snap
+sudo apt get install -y vim bucklespring 
+
+
+
+
+
+
+
+
+
+
+
