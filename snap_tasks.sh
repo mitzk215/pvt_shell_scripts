d8d143f5781966e345c8a395097f5491ef5a4acf
mitzk215 <35113893+mitzk215@users.noreply.github.com>
2025-04-24 17:38:46 +0000

    Update snap_tasks.sh

--- snap_tasks.sh
+++ snap_tasks.sh
@@ -3,3 +3,6 @@
 
 # THIS FILE CONTAINS ALL THE TAKS, SNAP HAS TO PERFORM !
 sudo snap refresh && sudo snap refresh firefox --channel latest/stable
+
+
+sudo snap install pycharm-educational --classic
