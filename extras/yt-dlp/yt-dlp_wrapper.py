b56ef4aa60a881ecfa017de9e316d7aeb4befc24
mitzk215 <35113893+mitzk215@users.noreply.github.com>
2025-08-21 22:17:51 +0530

    Create yt-dlp_wrapper.py

---
+++ extras/yt-dlp/yt-dlp_wrapper.py
@@ -0,0 +1,19 @@
+"""
+the downloader works
+playlist needed
+find options for inputing video links
+or loop through the links file one by one and download.
+
+
+github source code : https://github.com/yt-dlp/yt-dlp
+
++++++ After setting up python virtual environment and activating that environment, do these :
+"""
+
+wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
+
+# video downloads 
+yt-dlp <URL_of_the_youtube_video>
+
+## create a configuration file ...
+touch yt-dlp.conf
